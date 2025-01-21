5e7640611302247b9efd25ff3253f9f2e82a58f0
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-21 12:07:48 -0500

    Refactor home screen layout and enhance UI elements for better user experience

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,31 +11,36 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/loginbg.jpeg'),
-            fit: BoxFit.cover,
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/loginbg.jpeg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.7),
+              Colors.black.withOpacity(0.5),
+              Colors.transparent,
+            ],
           ),
         ),
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              colors: [
-                Colors.black.withOpacity(0.9),
-                Colors.black.withOpacity(0.7),
-                Colors.black.withOpacity(0.5),
-                Colors.transparent,
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 40, right: 20),
-            child: Column(
-              children: [
-                Align(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 40,
+                ),
+                child: Align(
                   alignment: Alignment.topRight,
                   child: ElevatedButton(
                     onPressed: () {
@@ -44,6 +49,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
                       elevation: 1.0,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
                     ),
                     child: const Text(
                       'Skip',
@@ -51,21 +58,119 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 40,
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              const Text(
+                'Welcome to',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'Welcome to',
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'D-LIVREM',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Color(0xff36B6B0),
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Text(
+                  'Your favourite foods delivered fast at your door.',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 200,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 1.0,
+                    ),
                   ),
-                )
-              ],
-            ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'sign in with',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Add your onPressed code here!
+                    },
+                    icon: Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/300/300221.png',
+                      height: 24.0,
+                    ),
+                    label: const Text(
+                      'Google',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      elevation: 1.0,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Add your onPressed code here!
+                    },
+                    icon: Image.network(
+                      'https://cdn-icons-png.flaticon.com/512/5968/5968764.png',
+                      height: 24.0,
+                    ),
+                    label: const Text(
+                      'Facebook',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      elevation: 1.0,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24.0, vertical: 12.0),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
-    );
+    ));
   }
 }
